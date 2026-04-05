a464b565f348ebcdb736fc7527b14ab327389c40
Valerii Burmistrov <chrisburrrma@gmail.com>
2026-04-05 22:53:41 +0200

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        UnionFind uf = new UnionFind(edges.length);
+        for (int[] edge: edges) {
+            if (!uf.unite(edge[0] - 1, edge[1] - 1)) {
+                return edge;
+            }
+        }
+
+        return new int[]{};
+    }
+
+    private class UnionFind {
+        private int[] parent;
+        private int[] rank;
+        
+        UnionFind(int n) {
+            rank = new int[n];
+            parent = new int[n];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+        }
+
+        boolean unite(int a, int b) {
+            int aRoot = findRoot(a);
+            int bRoot = findRoot(b);
+
+            if (aRoot == bRoot) {
+                return false;
+            }
+
+            if (rank[aRoot] < rank[bRoot]) {
+                parent[bRoot] = aRoot;
+            } else if (rank[aRoot] > rank[bRoot]) {
+                parent[aRoot] = bRoot;
+            } else {
+                parent[aRoot] = bRoot;
+                rank[aRoot]++;
+            }
+
+            return true;
+        }
+
+        private int findRoot(int node) {
+            if (node != parent[node]) {
+                parent[node] = findRoot(parent[node]);
+            }
+            return parent[node];
+        }
+    }
+}
