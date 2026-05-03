3e5a1f241d582445b7f914d85c266ea1f01c0ab5
Valerii Burmistrov <chrisburrrma@gmail.com>
2026-05-03 22:10:16 +0200

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i <= target; i++) {
+            for (int num: nums) {
+                if (num > i) {
+                    break;
+                }
+
+                dp[i] += dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
