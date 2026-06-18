e184892358aaf77daa67fdba32537cc1f4420554
Valerii Burmistrov <chrisburrrma@gmail.com>
2026-06-18 12:38:30 +0200

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        int n = strs.length;
+
+        for (int i = 0; i < Math.min(strs[0].length(), strs[n - 1].length()); i++) {
+            if (strs[0].charAt(i) != strs[n - 1].charAt(i)) {
+                return strs[0].substring(0, i);
+            }
+        }
+
+        return strs[0];
+    }
+}
