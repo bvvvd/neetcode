bb641e3c1b87f91d2092cae18f2b939f6197e876
Valerii Burmistrov <chrisburrrma@gmail.com>
2026-09-14 23:56:17 +0200

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val frequencies = mutableMapOf<Int, Int>()
+        nums.forEach {
+            frequencies.merge(it, 1, Int::plus)
+        }
+        val groupedFrequents = Array<MutableList<Int>>(nums.size + 1) {mutableListOf<Int>()}
+        frequencies.forEach {
+            (num, frequency) -> groupedFrequents[frequency].add(num)
+        }
+        val topFrequent = IntArray(k)
+        var n = k
+        for (i in nums.size downTo 1) {
+            groupedFrequents[i].forEach {
+                topFrequent[n - 1] = it
+                n--
+                if (n == 0) {
+                    return topFrequent
+                }
+            }
+        }
+
+        return topFrequent
+    }
+}
