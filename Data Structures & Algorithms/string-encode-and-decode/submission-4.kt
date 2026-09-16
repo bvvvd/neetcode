50e6c61cde79e18ffa3afc307295b2b83fcbe5c5
Valerii Burmistrov <chrisburrrma@gmail.com>
2026-09-16 10:39:25 +0200

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.kt
@@ -0,0 +1,28 @@
+class Solution {
+
+    fun encode(strs: List<String>): String {
+        val builder = StringBuilder()
+        strs.forEach {
+            builder.append(it.length)
+                    .append('#')
+                    .append(it)
+        }
+        return builder.toString()
+    }
+
+    fun decode(str: String): List<String> {
+        val strs = mutableListOf<String>()
+        var i = 0
+        while (i < str.length) {
+            var length = 0
+            while (str[i] != '#') {
+                length = length * 10 + (str[i] - '0')
+                i++
+            }
+            i++
+            strs.add(str.substring(i, i + length))
+            i += length 
+        }
+        return strs
+    }
+}
