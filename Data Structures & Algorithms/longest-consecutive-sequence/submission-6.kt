7ed8040da78bc084da87043aa3bea02749675568
Valerii Burmistrov <chrisburrrma@gmail.com>
2026-09-16 18:35:18 +0200

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        val set = nums.toSet()
+
+        var maxLength = 0
+        for (num in nums) {
+            if (num - 1 !in set) {
+                var sequenceElement = num
+                var length = 0
+
+                while (sequenceElement in set) {
+                    sequenceElement++
+                    length++
+                }
+
+                maxLength = max(length, maxLength)
+            }
+        }
+
+        return maxLength
+    }
+}
