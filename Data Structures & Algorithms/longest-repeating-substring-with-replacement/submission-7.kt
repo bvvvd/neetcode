cea0ec139ed077e4be5fa2908afc6199c9a441c1
Valerii Burmistrov <chrisburrrma@gmail.com>
2026-09-19 14:52:19 +0200

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        val window = IntArray(26)
+        var left = 0
+        var maxLength = 0
+        var maxFrequency = 0
+
+        for (right in s.indices) {
+            window[s[right] - 'A']++
+            maxFrequency = maxOf(maxFrequency, window[s[right] - 'A'])
+            while (right - left + 1 - maxFrequency > k) {
+                window[s[left] - 'A']--
+                left++
+            }
+
+            maxLength = maxOf(maxLength, right - left + 1)
+        }
+
+        return maxLength
+    }
+
+    private fun exceeds(window: IntArray, k: Int): Boolean {
+        var max = 0
+        var sum = 0
+        for (frequency in window) {
+            max = maxOf(max, frequency)
+            sum += frequency
+        }
+
+        return sum - max > k
+    }
+}
